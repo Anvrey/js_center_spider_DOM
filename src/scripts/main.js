fc285e4260dedda69693fbfee67e9e0b98952948
Andrey <inokatre08@gmail.com>
2025-11-17 00:14:33 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const SpiderLeft = (wallWidth - spiderWidth) / 2;
+const SpiderTop = (wallHeight - spiderHeight) / 2;
 
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+spider.style.top = `${SpiderTop}px`;
+spider.style.left = `${SpiderLeft}px`;
